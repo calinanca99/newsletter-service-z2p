8d667f9db431b65b17743c070a6630e0f45dfb94
calinanca99 <calinanca99@gmail.com>
2023-07-11 08:58:14 +0300

    Wrap DB password in Secret

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod test_utils {
     use std::net::TcpListener;
 
     use once_cell::sync::Lazy;
+    use secrecy::ExposeSecret;
     use sqlx::{Connection, Executor, PgConnection, PgPool};
     use uuid::Uuid;
 
@@ -64,16 +65,17 @@ pub mod test_utils {
     }
 
     async fn configure_database(config: &DatabaseSettings) -> PgPool {
-        let mut connection = PgConnection::connect(&config.connection_string_without_db())
-            .await
-            .expect("Failed to connect to Postgres.");
+        let mut connection =
+            PgConnection::connect(&config.connection_string_without_db().expose_secret())
+                .await
+                .expect("Failed to connect to Postgres.");
 
         connection
             .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
             .await
             .expect("Failed to create database.");
 
-        let connection_pool = PgPool::connect(&config.connection_string())
+        let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
             .await
             .expect("Failed to connect to Postgres.");
         sqlx::migrate!("./migrations")
